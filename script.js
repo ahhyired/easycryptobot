323a41bd8979aeead04d8bc9e89354ae46ae41d5
ahhyired <148357598+ahhyired@users.noreply.github.com>
2024-06-17 20:51:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'PONKE TON',
             description: 'Присоединяйтесь к дропу от проекта Ponke TON и получайте 700.000 $PONKE + 3 Ponke TON NFT (4000$)!',
             detailDescription: 'Detailed description of PONKE TON.',
-            icon: 'easycryptobot/images/ponke.jpg', // Place the correct path to the bot icon
+            icon: 'https://ahhyired.github.io/easycryptobot/images/ponke.jpg',
             link: 'https://t.me/PONKETONBOT'
         },
         // Add more bot objects here
     ];
 
     const botsList = document.querySelector('.bots-list');
-    const botDetail = document.querySelector('.bot-detail');
-    const botName = document.querySelector('.bot-name');
-    const botDescription = document.querySelector('.bot-description');
-    const botLink = document.querySelector('.bot-link');
-    const backButton = document.querySelector('.back-button');
-    const mainPage = document.querySelector('.main-page');
 
     bots.forEach(bot => {
         const botElement = document.createElement('div');
@@ -28,18 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
         `;
 
         botElement.addEventListener('click', () => {
-            botName.textContent = bot.name;
-            botDescription.textContent = bot.detailDescription;
-            botLink.href = bot.link;
-            mainPage.classList.add('hidden');
-            botDetail.classList.remove('hidden');
+            window.open(bot.link, '_blank');
         });
 
         botsList.appendChild(botElement);
-    });
-
-    backButton.addEventListener('click', () => {
-        botDetail.classList.add('hidden');
-        mainPage.classList.remove('hidden');
     });
 });
